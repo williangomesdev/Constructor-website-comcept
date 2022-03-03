f30a13aa80ffd2b53b3203e47b4b19ded5fcfe82
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-03 14:53:13 -0300

    Clients Slider

--- js/slides.js
+++ js/slides.js
@@ -8,7 +8,7 @@ var swiper = new Swiper(".home-slider", {
 });
 
 //Slide Avaliações
-var swiper = new Swiper(".reviews-slider", {
+var swiper = new Swiper(".reviews-slider, .blogs-slider", {
   loop: true,
   grabCursor: true,
   spaceBetween: 20,
@@ -19,14 +19,15 @@ var swiper = new Swiper(".reviews-slider", {
   },
 });
 
-//Slide Blogs
-var swiper = new Swiper(".blogs-slider", {
+//Slide Clientes
+var swiper = new Swiper(".logo-slider", {
   loop: true,
   grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
-    640: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    1024: { slidesPerView: 3 },
+    450: { slidesPerView: 2 },
+    640: { slidesPerView: 3 },
+    768: { slidesPerView: 4 },
+    1024: { slidesPerView: 5 },
   },
 });
