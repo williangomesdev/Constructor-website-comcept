b661799eb7cfe92e9fd1d90ef854438aaadf9e51
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-02 22:31:36 -0300

    reviews slide completed

--- js/slides.js
+++ js/slides.js
@@ -6,3 +6,15 @@ var swiper = new Swiper(".home-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//Slide Avaliações
+var swiper = new Swiper(".reviews-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    640: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
